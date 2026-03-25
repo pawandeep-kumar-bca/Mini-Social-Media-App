ef9346886de61b54f904ead396fd0c8b380b3479
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-03-25 14:47:42 +0530

    add signup validation using express-validator

---
+++ backend/src/middlewares/authValidation.middleware.js
@@ -0,0 +1,34 @@
+const { body, validationResult } = require("express-validator");
+
+const ResponseWithErrors = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+const signupUserValidation = [
+  body("username")
+    .notEmpty()
+    .withMessage("user name is required")
+    .trim()
+    .isLowercase(),
+  body("email").isEmail().notEmpty().withMessage("email is required"),
+  body(password)
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 6 })
+    .withMessage("password length is 6 letter")
+    .matches(/[A-Z]/)
+    .withMessage("Must contain uppercase")
+    .matches(/[a-z]/)
+    .withMessage("Must contain lowercase")
+    .matches(/[0-9]/)
+    .withMessage("Must contain number")
+    .matches(/[^A-Za-z0-9]/)
+    .withMessage("Must contain special char"),
+  ResponseWithErrors,
+];
+
+module.exports = {signupUserValidation}
