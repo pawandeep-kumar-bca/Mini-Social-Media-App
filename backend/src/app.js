68a2d16f19672ca7a8e07c5b5cd455dd76d3e859
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-03-25 15:47:43 +0530

    create post model and post router

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 
 const authRouter = require('./routes/auth.routes')
-
+const postRouter = require('./routes/post.routes')
 app.use('api/v1/auth',authRouter)
+app.use('api/v1/posts',postRouter)
 module.exports = app
---
+++ backend/src/models/post.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", 
+      required: true,
+    },
+
+    text: {
+      type: String,
+      trim: true,
+    },
+
+    image: {
+      type: String, 
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: [commentSchema],
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const postModel = new mongoose.model("Post", postSchema);
+
+module.exports = postModel
