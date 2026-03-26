5bfa02c967a2aafdd24a4fc0c7c7a3197b71df88
pawandeep kumar <pawandeepkumar7634@gmail.com>
2026-03-26 14:17:48 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const app = require("./src/app");
 const connectDB = require('./src/config/db')
 
 connectDB()
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
